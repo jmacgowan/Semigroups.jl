b77fa34570fc8e9597a21aba3f3d782a241ca8b8
Joseph Macgowan <macgowan.joseph@gmail.com>
2026-09-21 11:08:53 +0100

    simplify tests

--- test/test_to_presentation.jl
+++ test/test_to_presentation.jl
@@ -10,10 +10,12 @@ using Semigroups
     @testset "conversion with no rules" begin
         p = Presentation()
         set_alphabet!(p, 3)
+        k = Kambites(twosided, p)
 
-        q = Semigroups.to(Kambites(twosided, p))
+        q = Semigroups.to(k)
 
         @test q isa Presentation
+        @test q == presentation(k)
         @test alphabet(q) == alphabet(p)
         @test rules(q) == rules(p)
     end
@@ -23,9 +25,11 @@ using Semigroups
         set_alphabet!(p, 3)
         add_rule_no_checks!(p, [1, 2, 1], [2, 3])
         add_rule_no_checks!(p, [3, 3], [1])
+        k = Kambites(twosided, p)
 
-        q = Semigroups.to(Kambites(twosided, p))
+        q = Semigroups.to(k)
 
+        @test q == presentation(k)
         @test q == p
         @test alphabet(q) == alphabet(p)
         @test rules(q) == rules(p)
@@ -35,22 +39,24 @@ using Semigroups
         p = Presentation()
         set_alphabet!(p, 1)
         set_contains_empty_word!(p, true)
+        k = Kambites(twosided, p)
 
-        q = Semigroups.to(Kambites(twosided, p))
+        q = Semigroups.to(k)
 
+        @test q == presentation(k)
         @test contains_empty_word(q)
     end
 
-    @testset "copy semantics" begin
+    @testset "matches Kambites presentation" begin
         p = Presentation()
         set_alphabet!(p, 2)
         add_rule_no_checks!(p, [1, 1], [2])
+        k = Kambites(twosided, p)
 
-        q = Semigroups.to(Kambites(twosided, p))
-        add_rule_no_checks!(q, [2, 2], [1])
+        q = Semigroups.to(k)
 
-        @test q !== p
-        @test length(rules(p)) == 1
-        @test length(rules(q)) == 2
+        @test q == presentation(k)
+        @test alphabet(q) == alphabet(presentation(k))
+        @test rules(q) == rules(presentation(k))
     end
 end
